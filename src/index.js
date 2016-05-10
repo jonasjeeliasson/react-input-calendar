c46a6cf7fa31ce20c2f5e830bf13278c659b2464
Jonas Eliasson <jonas.eliasson@comhem.se>
2016-05-10 10:14:54 +0200

    Merge latest src/index.js from original repo (commit eed55009e367bd122373bb97f29239d499fae1f8)

--- src/index.js
+++ src/index.js
@@ -9,46 +9,19 @@ import MonthsView from './month-view'
 import YearsView from './year-view'
 import Util from './util'
 
-const Calendar = React.createClass({
-  propTypes: {
-    closeOnSelect: React.PropTypes.bool,
-    computableFormat: React.PropTypes.string,
-    strictDateParsing: React.PropTypes.bool,
-    parsingFormat: React.PropTypes.oneOfType([
-      React.PropTypes.string,
-      React.PropTypes.arrayOf(React.PropTypes.string)
-    ]),
-    date: React.PropTypes.any,
-    minDate: React.PropTypes.any,
-    maxDate: React.PropTypes.any,
-    format: React.PropTypes.string,
-    inputName: React.PropTypes.string,
-    inputFieldId: React.PropTypes.string,
-    inputFieldClass: React.PropTypes.string,
-    minView: React.PropTypes.number,
-    onBlur: React.PropTypes.func,
-    onChange: React.PropTypes.func,
-    onChangeAndBlur: React.PropTypes.func,
-    openOnInputFocus: React.PropTypes.bool,
-    placeholder: React.PropTypes.string,
-    hideTouchKeyboard: React.PropTypes.bool,
-    hideIcon: React.PropTypes.bool,
-    customIcon: React.PropTypes.string,
-    todayText: React.PropTypes.string
-  },
+class Calendar extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+    const date = props.date ? moment(Util.toDate(props.date)) : null
+    const minDate = props.minDate ? moment(Util.toDate(props.minDate)) : null
+    const maxDate = props.maxDate ? moment(Util.toDate(props.maxDate)) : null
+    const format = props.format || 'MM-DD-YYYY'
+    const minView = parseInt(props.minView, 10) || 0
+    const computableFormat = props.computableFormat || 'MM-DD-YYYY'
+    const strictDateParsing = props.strictDateParsing || false
+    const parsingFormat = props.parsingFormat || format
 
-
-  getInitialState() {
-    const date = this.props.date ? moment(Util.toDate(this.props.date)) : null
-    const minDate = this.props.minDate ? moment(Util.toDate(this.props.minDate)) : null
-    const maxDate = this.props.maxDate ? moment(Util.toDate(this.props.maxDate)) : null
-    const format = this.props.format || 'MM-DD-YYYY'
-    const minView = parseInt(this.props.minView, 10) || 0
-    const computableFormat = this.props.computableFormat || 'MM-DD-YYYY'
-    const strictDateParsing = this.props.strictDateParsing || false
-    const parsingFormat = this.props.parsingFormat || format
-
-    return {
+    this.state = {
       date,
       minDate,
       maxDate,
@@ -62,96 +35,43 @@ const Calendar = React.createClass({
       strictDateParsing,
       parsingFormat
     }
-  },
+  }
 
   componentDidMount() {
     document.addEventListener('click', this.documentClick)
-  },
+  }
 
   componentWillReceiveProps(nextProps) {
-    const date = nextProps.date ? moment(Util.toDate(nextProps.date)) : null
-    const inputValue = date && date.isValid() ? date.format(this.state.format) : nextProps.date
+
     this.setState({
-      date: date ? date : this.state.date,
-      inputValue: date ? inputValue : null
+      date: nextProps.date ? moment(Util.toDate(nextProps.date)) : this.state.date,
+      inputValue: nextProps.date
+        ? moment(Util.toDate(nextProps.date)).format(this.state.format) : null,
+        isVisible: nextProps.disabled === true
     })
-  },
+  }
 
   componentWillUnmount() {
     document.removeEventListener('click', this.documentClick)
-  },
+  }
 
-  setVisibility(val) {
-    const value = val !== undefined ? val : !this.state.isVisible
-    const eventMethod = value ? 'addEventListener' : 'removeEventListener'
-
-    document[eventMethod]('keydown', this.keyDown)
-
-    if (this.state.isVisible !== value) {
-      this.setState({ isVisible: value })
-    }
-  },
-
-  setDate(date, isDayView) {
-    if (this.checkIfDateDisabled(date)) return
-
-    const calendarClosed = this.props.closeOnSelect && isDayView
-
-    this.setState({
-      date,
-      inputValue: date.format(this.state.format),
-      isVisible: calendarClosed ? !this.state.isVisible : this.state.isVisible
-    })
-
-    if (calendarClosed && this.props.onChangeAndBlur) {
-      this.props.onChangeAndBlur(date.format(this.state.computableFormat))
-    } else if (this.props.onChange) {
-      this.props.onChange(date.format(this.state.computableFormat))
-    }
-  },
+  changeDate = e => { //eslint-disable-line
+    this.setState({ inputValue: e.target.value })
+  }
 
   checkIfDateDisabled(date) {
     return date && this.state.minDate && date.isBefore(this.state.minDate, 'day')
       || date && this.state.maxDate && date.isAfter(this.state.maxDate, 'day')
-  },
+  }
 
-  nextView() {
-    if (this.checkIfDateDisabled(this.state.date)) return
-    this.setState({ currentView: ++this.state.currentView })
-  },
-
-  prevView(date) {
-    let newDate = date
-    if (this.state.minDate && date.isBefore(this.state.minDate, 'day')) {
-      newDate = this.state.minDate.clone()
+  documentClick = () => {
+    if (!this.state.isCalendar) {
+      this.setVisibility(false)
     }
+    this.setState({ isCalendar: false })
+  }
 
-    if (this.state.maxDate && date.isAfter(this.state.maxDate, 'day')) {
-      newDate = this.state.maxDate.clone()
-    }
-
-    if (this.state.currentView === this.state.minView) {
-      this.setState({
-        date: newDate,
-        inputValue: date.format(this.state.format),
-        isVisible: false
-      })
-      if (this.props.onChange) {
-        this.props.onChange(date.format(this.state.computableFormat))
-      }
-    } else {
-      this.setState({
-        date,
-        currentView: --this.state.currentView
-      })
-    }
-  },
-
-  changeDate(e) {
-    this.setState({ inputValue: e.target.value })
-  },
-
-  inputBlur(e) {
+  inputBlur = e => {
     let newDate = null
     let computableDate = null
     const date = this.state.inputValue
@@ -187,21 +107,75 @@ const Calendar = React.createClass({
     if (this.props.onBlur) {
       this.props.onBlur(e, computableDate)
     }
-  },
+  }
 
+  keyDown = e => {
+    Util.keyDownActions.call(this, e.keyCode)
+  }
 
-  documentClick() {
-    if (!this.isCalendar) {
-      this.setVisibility(false)
+  nextView = () => {
+    if (this.checkIfDateDisabled(this.state.date)) return
+    this.setState({ currentView: ++this.state.currentView })
+  }
+
+  prevView = date => {
+    let newDate = date
+    if (this.state.minDate && date.isBefore(this.state.minDate, 'day')) {
+      newDate = this.state.minDate.clone()
     }
-    this.isCalendar = false
-  },
 
-  calendarClick() {
-    this.isCalendar = true
-  },
+    if (this.state.maxDate && date.isAfter(this.state.maxDate, 'day')) {
+      newDate = this.state.maxDate.clone()
+    }
 
-  todayClick() {
+    if (this.state.currentView === this.state.minView) {
+      this.setState({
+        date: newDate,
+        inputValue: date.format(this.state.format),
+        isVisible: false
+      })
+      if (this.props.onChange) {
+        this.props.onChange(date.format(this.state.computableFormat))
+      }
+    } else {
+      this.setState({
+        date,
+        currentView: --this.state.currentView
+      })
+    }
+  }
+
+  setDate = (date, isDayView) => {
+    if (this.checkIfDateDisabled(date)) return
+
+    this.setState({
+      date,
+      inputValue: date.format(this.state.format),
+      isVisible: this.props.closeOnSelect
+        && isDayView ? !this.state.isVisible : this.state.isVisible
+    })
+
+    if (this.props.onChange) {
+      this.props.onChange(date.format(this.state.computableFormat))
+    }
+  }
+
+  setVisibility(val) {
+    const value = val !== undefined ? val : !this.state.isVisible
+    const eventMethod = value ? 'addEventListener' : 'removeEventListener'
+
+    document[eventMethod]('keydown', this.keyDown)
+
+    if (this.state.isVisible !== value && !this.props.disabled) {
+      this.setState({ isVisible: value })
+    }
+  }
+
+  calendarClick = () => {
+    this.setState({ isCalendar: true })
+  }
+
+  todayClick = () => {
     const today = moment().startOf('day')
 
     if (this.checkIfDateDisabled(today)) return
@@ -215,35 +189,29 @@ const Calendar = React.createClass({
     if (this.props.onChange) {
       this.props.onChange(today.format(this.state.computableFormat))
     }
-  },
+  }
 
-  toggleClick() {
-    this.isCalendar = true
+  toggleClick = () => {
+    this.setState({ isCalendar: true })
     this.setVisibility()
-  },
-
-  keyDown(e) {
-    Util.keyDownActions.call(this, e.keyCode)
-  },
-
-  isCalendar: false,
+  }
 
   render() {
     // its ok for this.state.date to be null, but we should never
     // pass null for the date into the calendar pop up, as we want
     // it to just start on todays date if there is no date set
-    let calendarDate = this.state.date && this.state.date.isValid() ? this.state.date : moment()
+    let calendarDate = this.state.date || moment()
     let view
 
     switch (this.state.currentView) {
       case 0:
-        view = <DaysView
+        view = (<DaysView
           date={calendarDate}
           nextView={this.nextView}
           maxDate={this.state.maxDate}
           minDate={this.state.minDate}
           setDate={this.setDate}
-        />
+        />)
         break
       case 1:
         view = (<MonthsView
@@ -284,9 +252,10 @@ const Calendar = React.createClass({
       <div className={calendarClass} onClick={this.calendarClick}>
         {view}
         <span
-          className={'today-btn' + (this.checkIfDateDisabled(moment().startOf('day')) ? ' disabled' : '')}
-          onClick={this.todayClick}
-        >
+          className={
+            `today-btn${this.checkIfDateDisabled(moment().startOf('day')) ? ' disabled' : ''}`
+          }
+          onClick={this.todayClick}>
           {todayText}
         </span>
       </div>
@@ -296,16 +265,19 @@ const Calendar = React.createClass({
     if (this.props.hideTouchKeyboard) {
       // do not break server side rendering:
       try {
-        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i
+          .test(navigator.userAgent)) {
           readOnly = true
         }
-      } catch (e) {}
+      } catch (e) {
+        console.warn(e) //eslint-disable-line
+      }
     }
 
     let calendarIcon
     if (this.props.customIcon == null) {
       // Do not show calendar icon if hideIcon is true
-      calendarIcon = this.props.hideIcon ? '' :
+      calendarIcon = this.props.hideIcon || this.props.disabled ? '' :
         <span className="icon-wrapper calendar-icon" onClick={this.toggleClick} >
           <svg width="16" height="16" viewBox="0 0 16 16">
             <path d="M5 6h2v2h-2zM8 6h2v2h-2zM11 6h2v2h-2zM2 12h2v2h-2zM5
@@ -337,6 +309,7 @@ const Calendar = React.createClass({
           onFocus={this.props.openOnInputFocus ? this.toggleClick : ''}
           placeholder={this.props.placeholder}
           readOnly={readOnly}
+          disabled={this.props.disabled}
           type="text"
           value={this.state.inputValue}
         />
@@ -345,6 +318,33 @@ const Calendar = React.createClass({
       </div>
     )
   }
-})
+}
+
+Calendar.propTypes = {
+  closeOnSelect: React.PropTypes.bool,
+  computableFormat: React.PropTypes.string,
+  strictDateParsing: React.PropTypes.bool,
+  parsingFormat: React.PropTypes.oneOfType([
+    React.PropTypes.string,
+    React.PropTypes.arrayOf(React.PropTypes.string)
+  ]),
+  date: React.PropTypes.any,
+  minDate: React.PropTypes.any,
+  maxDate: React.PropTypes.any,
+  format: React.PropTypes.string,
+  inputName: React.PropTypes.string,
+  inputFieldId: React.PropTypes.string,
+  inputFieldClass: React.PropTypes.string,
+  minView: React.PropTypes.number,
+  onBlur: React.PropTypes.func,
+  onChange: React.PropTypes.func,
+  openOnInputFocus: React.PropTypes.bool,
+  placeholder: React.PropTypes.string,
+  hideTouchKeyboard: React.PropTypes.bool,
+  hideIcon: React.PropTypes.bool,
+  customIcon: React.PropTypes.string,
+  todayText: React.PropTypes.string,
+  disabled: React.PropTypes.bool
+}
 
 export default Calendar
